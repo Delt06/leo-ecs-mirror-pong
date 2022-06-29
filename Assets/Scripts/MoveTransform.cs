806d4112e38679849b0a555ca21b1d74f4b3582a
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-06-29 19:30:59 +0300

    Add client-side interpolation

--- Assets/Scripts/MoveTransform.cs
+++ Assets/Scripts/MoveTransform.cs
@@ -3,32 +3,58 @@ using UnityEngine;
 
 public class MoveTransform : MonoBehaviour
 {
+    public float TickTime = 0.1f;
+    public float InterpolateTime = 0.1f;
+    public float SnapTime = 0.2f;
+    public bool Extrapolate = true;
+    
+    private Vector3 _lastVelocity;
+
     private GameObject _o;
+    private float _timeSinceLastFrame;
+    private Vector3 _serverPosition;
 
     private void Start()
     {
         _o = GameObject.CreatePrimitive(PrimitiveType.Cube);
         NetworkClient.RegisterHandler<MoveTransformCommand>(OnMoved, false);
         NetworkServer.RegisterHandler<JumpCommand>(OnJumped);
+
+        InvokeRepeating(nameof(SimulateOnServer), 0, TickTime);
     }
 
     private void Update()
     {
         if (NetworkClient.active && NetworkClient.isConnected)
             OnClient();
-
-        if (NetworkServer.active || NetworkClient.isHostClient)
-            OnServer();
     }
 
     private void OnClient()
     {
+        _timeSinceLastFrame += Time.deltaTime;
+        _o.transform.position = Interpolate(_o.transform.position, _serverPosition);
+
         if (Input.GetButtonDown("Jump"))
         {
             JumpLocally();
             NetworkClient.Send(new JumpCommand());
         }
     }
+
+    private Vector3 Interpolate(Vector3 current, Vector3 target)
+    {
+        if (_timeSinceLastFrame > SnapTime)
+            return target;
+        var t = _timeSinceLastFrame / InterpolateTime;
+        return Extrapolate ? Vector3.LerpUnclamped(current, target, t) : Vector3.Lerp(current, target, t);
+    }
+
+    private void SimulateOnServer()
+    {
+        if (NetworkServer.active || NetworkClient.isHostClient)
+            OnServer();
+    }
+
 
     private static void OnServer()
     {
@@ -52,6 +78,7 @@ public class MoveTransform : MonoBehaviour
 
     private void OnMoved(MoveTransformCommand obj)
     {
-        _o.transform.position = obj.Position;
+        _timeSinceLastFrame = 0f;
+        _serverPosition = obj.Position;
     }
 }
