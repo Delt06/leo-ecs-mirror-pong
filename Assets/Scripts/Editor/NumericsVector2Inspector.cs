a5509f6031ced5fdf4f260b4b8602096cb9bc8f4
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-07-07 10:08:27 +0300

    Add NumericsVector2Inspector

---
+++ Assets/Scripts/Editor/NumericsVector2Inspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using JetBrains.Annotations;
+using Leopotam.EcsLite.UnityEditor;
+using Simulation;
+using UnityEditor;
+
+namespace Editor
+{
+    [UsedImplicitly]
+    internal sealed class NumericsVector2Inspector : EcsComponentInspectorTyped<Vector2>
+    {
+        public override bool OnGuiTyped(string label, ref Vector2 value, EcsEntityDebugView entityView)
+        {
+            var newValue = EditorGUILayout.Vector2Field(label, value.ToUnityVector()).ToNumericsVector();
+            if (newValue == value) return false;
+            value = newValue;
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Simulation/VectorExtensions.cs
+++ Assets/Scripts/Simulation/VectorExtensions.cs
@@ -4,6 +4,7 @@ namespace Simulation
 {
     public static class VectorExtensions
     {
-        public static Vector2 ToUnityVector(this System.Numerics.Vector2 vector) => new Vector2(vector.X, vector.Y);
+        public static Vector2 ToUnityVector(this System.Numerics.Vector2 vector) => new(vector.X, vector.Y);
+        public static System.Numerics.Vector2 ToNumericsVector(this Vector2 vector) => new(vector.x, vector.y);
     }
 }
